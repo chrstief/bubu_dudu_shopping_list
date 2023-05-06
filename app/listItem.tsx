82ca69a008ba3074174abea71a18019e4c28934d
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-05-06 18:48:59 +0000

    fix unintended submits

--- app/listItem.tsx
+++ app/listItem.tsx
@@ -7,8 +7,8 @@ export function ListItem({ item }: { item: string }) {
 
   return (
     <button
+      type="button"
       onClick={(e) => {
-        e.preventDefault(); //prevent the forms action from firing
         startTransition(() => removeItem(item));
       }}
       className="bg-stone-100 hover:bg-amber-100 rounded-lg p-4 text-center grow shadow-md"
