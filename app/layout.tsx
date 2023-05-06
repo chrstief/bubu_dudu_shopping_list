664dbf0cc66b3ecc103a3413466a2cb0543694df
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-05-06 14:33:05 +0000

    styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Rubik } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Rubik({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata = {
   title: 'Bubu Dudu Shopping List',
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { revalidatePath } from "next/cache";
 import Image from "next/image";
 
-let items = ["oranges", "minced meat", "milk", "kiwis"];
+let items = ["Oranges", "Minced Meat", "Milk", "Kiwis"];
 
 export default function Home() {
   async function addItem(data: FormData) {
@@ -14,18 +14,36 @@ export default function Home() {
   }
 
   return (
-    <form action={addItem}>
+    <form
+      action={addItem}
+      className="flex flex-col gap-4 items-center max-w-sm mx-auto mt-14"
+    >
       <Image
         src="/aac829889e740dbeece2cf04649f2a46-fotor-bg-remover-20230506134539.png"
         alt=""
-        width="64"
-        height="64"
+        width="300"
+        height="300"
       />
-      <input name="item" type="text" placeholder="bread" />
-      <button type="submit">Add to cart</button>
-      <ul>
+      <input
+        name="item"
+        type="text"
+        placeholder="Bread"
+        className="bg-gray-300 text-gray-700 rounded-lg p-4 w-full text-center"
+      />
+      <button
+        type="submit"
+        className="bg-rose-800 text-gray-100 rounded-lg p-4 w-full"
+      >
+        Add to cart
+      </button>
+      <ul className="flex gap-4 flex-wrap">
         {items.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li
+            key={index}
+            className="bg-stone-100 rounded-lg p-4 text-center grow shadow-md"
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </form>
