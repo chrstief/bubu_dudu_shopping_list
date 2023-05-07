fb8f9625f922c9ef4cfe668585e20725c754570c
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-05-07 18:11:29 +0000

    use next.js image optimizations

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { addItem } from "./actions";
 import { ListItem } from "./listItem";
+import heroImage from "./assets/aac829889e740dbeece2cf04649f2a46-fotor-bg-remover-20230506134539.png";
 
 let items = ["Oranges", "Minced Meat", "Milk", "Kiwis"];
 
@@ -10,12 +11,7 @@ export default function Home() {
       action={addItem}
       className="flex flex-col gap-4 items-center max-w-sm mx-auto mt-14"
     >
-      <Image
-        src="/aac829889e740dbeece2cf04649f2a46-fotor-bg-remover-20230506134539.png"
-        alt=""
-        width="300"
-        height="300"
-      />
+      <Image src={heroImage} alt="" width="300" height="300" />
       <input
         name="item"
         type="text"
@@ -30,10 +26,7 @@ export default function Home() {
       </button>
       <div className="flex gap-4 flex-wrap">
         {items.map((item, index) => (
-          <ListItem
-            item={item}
-            key={index}
-          />
+          <ListItem item={item} key={index} />
         ))}
       </div>
     </form>
