6f8a10951ef5a8e67e6838daec60d722a5c4088f
Christian Stief <65469300+chrstief@users.noreply.github.com>
2023-05-15 04:11:05 +0000

    submit on pressing enter

--- app/Input.tsx
+++ app/Input.tsx
@@ -6,6 +6,15 @@ export default function Input() {
   const [inputValue, setInputValue] = useState("");
   let [pending, startTransition] = useTransition();
 
+  function submit() {
+    if (inputValue) {
+      startTransition(() => {
+        addItem(inputValue);
+        setInputValue("");
+      });
+    }
+  }
+
   return (
     <>
       <input
@@ -14,18 +23,16 @@ export default function Input() {
         className="bg-gray-300 text-gray-700 rounded-lg p-4 w-full text-center"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            submit();
+          }
+        }}
       />
       <button
         disabled={pending}
         className="bg-rose-800 disabled:animate-pulse text-gray-100 rounded-lg p-4 w-full"
-        onClick={() => {
-          if (inputValue) {
-            startTransition(() => {
-              addItem(inputValue);
-              setInputValue("");
-            });
-          }
-        }}
+        onClick={submit}
       >
         Add to cart
       </button>
