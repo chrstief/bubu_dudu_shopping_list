c8b0b0df42d6a5d6c32fe286c1072d0b9050ab30
Christian Stief <chrstief@gmail.com>
2025-05-07 15:25:14 +0200

    implement optimistic updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
-import { ListItem } from "./listItem";
 import heroImage from "./assets/aac829889e740dbeece2cf04649f2a46-fotor-bg-remover-20230506134539.png";
 import { kv } from "@vercel/kv";
-import Input from "./Input";
+import { Items } from "./items";
 
 export default async function Home() {
   const items = await kv.smembers("shoppingList");
@@ -10,12 +9,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col gap-4 items-center max-w-sm mx-auto my-14 px-4">
       <Image src={heroImage} alt="" width="300" height="300" />
-      <Input />
-      <div className="flex gap-4 flex-wrap">
-        {items.sort().map((item, index) => (
-          <ListItem item={item} key={index} />
-        ))}
-      </div>
+      <Items items={items} />
     </div>
   );
 }
