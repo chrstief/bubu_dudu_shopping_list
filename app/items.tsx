1f830d8f4f73ef17e215dce9edbe8ce6b4a8155e
Christian Stief <chrstief@gmail.com>
2026-03-04 15:17:09 +0100

    start with kiwi

--- app/items.tsx
+++ app/items.tsx
@@ -5,6 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 const foodEmojis = [
+  "🥝",
   "🍎",
   "🍏",
   "🍊",
@@ -20,7 +21,6 @@ const foodEmojis = [
   "🥭",
   "🍍",
   "🥥",
-  "🥝",
   "🍅",
   "🫒",
   "🥑",
